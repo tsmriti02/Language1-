7e7234a814ff5416bb0c93ede57ec24b879f6847
tsmriti02 <tsmriti02@gmail.com>
2021-06-07 23:40:06 +0530

    variables

---
+++ Lesson2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//This is just for practice purpose
+/*For multi line comment
+use slash 
+and star 8*/
+int main(){
+    int sum=6
+    cout<< "Hello" << sum;
+    return 0;
+}
